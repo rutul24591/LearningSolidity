80baff291e47aa50a07d4c4b81c0423dbafb55eb
Rutul Amin <ramin24591@gmail.com>
2022-04-07 17:16:23 +0530

    Create first smart contract

---
+++ 1.NameAndAgeGetterAndSetterContract/NameAndAgeContract.sol
@@ -0,0 +1,23 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity ^0.7.0;
+
+contract NameAndAgeContract{
+    string private name;
+    uint private age;
+
+    function getAge() public view returns (uint){
+        return age;
+    }
+
+    function getName() public view returns (string memory){
+        return name;
+    }
+
+    function setAge(uint newAge) public {
+        age = newAge;
+    }
+
+    function setName(string memory newName) public {
+        name = newName;
+    }
+}
